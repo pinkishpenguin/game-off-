858fd4bc60232a4857d113ba681c488a5b7fb428
pinkishpenguin <72714651+pinkishpenguin@users.noreply.github.com>
2020-10-12 08:47:35 -0400

    Update movement.py

--- movement.py
+++ movement.py
@@ -1,23 +1,5 @@
 
-"""
-Sample Python/Pygame Programs
-Simpson College Computer Science
-http://programarcadegames.com/
-http://simpson.edu/computer-science/
- 
-From:
-http://programarcadegames.com/python_examples/f.php?file=platform_moving.py
- 
-Explanation video: http://youtu.be/YKdOD5VkY48
- 
-Part of a series:
-http://programarcadegames.com/python_examples/f.php?file=move_with_walls_example.py
-http://programarcadegames.com/python_examples/f.php?file=maze_runner.py
-http://programarcadegames.com/python_examples/f.php?file=platform_jumper.py
-http://programarcadegames.com/python_examples/f.php?file=platform_scroller.py
-http://programarcadegames.com/python_examples/f.php?file=platform_moving.py
-http://programarcadegames.com/python_examples/sprite_sheets/
-"""
+import file2
 import pygame
  
 # Global constants
